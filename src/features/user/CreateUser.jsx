6d77777b8f1809905cd985edacd16f73d46b7b73
unknown <parthrc00@gmail.com>
2023-12-29 12:16:30 +0530

    Started react redux - user

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import Button from "../../ui/Button.jsx";
+import { useDispatch } from "react-redux";
+import { updateName } from "../user/userSlice.js";
+import { useNavigate } from "react-router-dom";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username) return;
+
+    dispatch(updateName(username));
+    navigate("/menu");
   }
 
   return (
@@ -24,7 +35,7 @@ function CreateUser() {
 
       {username !== "" && (
         <div>
-          <Button type='primary'>Start ordering</Button>
+          <Button type="primary">Start ordering</Button>
         </div>
       )}
     </form>
